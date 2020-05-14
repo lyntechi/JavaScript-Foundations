62f779b66311942fccbeb52f39a7c446349172ae
lyntechi <lynda-santiago@lambdastudents.com>
2020-05-13 20:42:47 -0400

    finished part 1 and 1.5

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 // 🌟🌟🌟 M V P 🌟🌟🌟//
 
 // 🏡 Task 1: Variables
-/* Create variables for principal, interest rate, and years. Assign them the values 200000, 0.05, and 30 respectively. Create another value called name and give it the value of your own name.
+/* Create variables for principal, interest rate, and years. Assign them the values 200000, 0.05, and 30 respectively. Create another
+ value called name and give it the value of your own name.
 */
 
-
+let principal = 200000;
+let interestRate = 0.05;
+let years = 30;
+const name = 'lynda';
 
 
 
@@ -16,21 +20,27 @@ Create a variable called `monthlyInterestRate` and give it the value of interest
 Create another variable called `periods` and give it the value of years*12.
 */
 
-
+let monthlyInterestRate = interestRate / 12;
+let periods = years*12;
 
 
 // 🏡 Task 2: Harder Math
 /* Create your calculator! Use the formula in the ReadMe to run calculations on your numbers. Save the final value into a variable called monthlyRate.
 
-Hint: while these calculations can be done in one line, it might be helpful to create a variable called "numerator" to calculate the numerator, and another called "denominator" to calculate the denominator 
+Hint: while these calculations can be done in one line, it might be helpful to create a variable called "numerator" to calculate the numerator,
+ and another called "denominator" to calculate the denominator 
 
 Hint #2: you'll need to use the `math` object for parts of this calculation!
 
 When your math is correct, monthlyRate will equal 1073.64
 */
+let numerator = interestRate(Math.pow(1,interestRate))*periods;
+console.log(numerator);
+let denominator = Math.pow(1+interestRate)*periods -1;
+let monthlyRate = principal * numerator / denominator;
+(console.log(monthlyRate) ;
 
-
-
+// M = P [ I ( 1 + I )^N ] / [ ( 1 + I )^N – 1 ]
 
 // 🏡 Task 3: Function
 /* Create a function called `mortgageCalculator` that combines all of the steps from task 1 and 2 and returns a sentence "{Name}, your monthly rate is ${monthlyRate}"
